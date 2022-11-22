7070b4230dccf2556a52c4d400736fcbefb20ac6
Eduard Tesvintsev <tesvintsev@gmail.com>
2022-11-22 20:50:07 +0300

    initial project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './scss/app.scss';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Mexico" price="500" />
+            <PizzaBlock title="Diablo" price={999}/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
