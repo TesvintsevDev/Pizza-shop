9e7a5d689f57cabcba6311520975c1d3810e21aa
Edward <83923244+TesvintsevDev@users.noreply.github.com>
2024-05-04 23:49:56 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,7 @@ function App() {
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
 					{pizzas.map((obj) => (
-              <PizzaBlock {...obj}
-              />
+              <PizzaBlock {...obj} />
             ))}
           </div>
         </div>
